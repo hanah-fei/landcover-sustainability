8a9a35e7e0576af9eb608c465e71e1ea2ce66173
nikitamaia <namjoshi.nikita@gmail.com>
2019-10-25 16:06:05 -0700

    scripts to shuffle,split,load data

---
+++ build_dataset.py
@@ -0,0 +1,87 @@
+import shutil 
+import numpy
+import os
+
+random.seed(123)
+
+"""Assumes you have downloaded the 13 band tif data from 
+   http://madm.dfki.de/downloads with the following directory structure
+   where the sub-directory is the image label.
+   
+   data_dir/Forest/Forest_864.tif
+   data_dir/Forest/Forest_2917.tif
+   ...
+   data_dir/Pasture/Pasture_1212.tif
+   data_dir/Pasture/Pasture_276.tif
+   data_dir/Pasture/Pasture_1574.tif 
+
+   This script creates a 80/10/10 split of the (shuffled) data
+   and writes the corresponding image names to a train, dev, and
+   test folder.
+
+   out_dir/train/PermanentCrop_1198.tif
+   out_dir/train/'Residential_1497.tif
+   ...
+   out_dir/dev/HerbaceousVegetation_919.tif
+   out_dir/dev/River_1066.tif
+   ...
+   out_dir/test/AnnualCrop_2173.tif
+   out_dir/test/Highway_1678.tif
+"""
+
+
+## Set your data_dir and out_dir
+data_dir = '../satellite_data/all_bands/'
+out_dir = '../satellite_data/'
+
+
+def main(data_dir, out_dir):
+  filenames = get_and_shuffle(data_dir)
+  train_images, dev_images, test_images = create_splits(filenames)
+  for split in ['train', 'dev', 'test']:
+    output_dir_split = os.path.join(out_dir, split)
+    if not os.path.exists(output_dir_split):
+      os.mkdir(output_dir_split)
+    else:
+      print("Warning: dir {} already exists".format(output_dir_split))
+  
+  print("Copying train data.")
+  write_data(train_images, 'train')
+  print("Copying dev data.")
+  write_data(dev_images, 'dev')
+  print("Copying test data.")
+  write_data(test_images, 'test')   
+
+ 
+def write_data(filenames, split):
+  """Copy images to new outdir."""
+  for name in filenames:
+    source = data_dir + name.split('_')[0] + '/' + name
+    destination = out_dir + split + '/' + name
+    dest = shutil.copyfile(source, destination)  
+ 
+
+def create_splits(filenames):
+  """Split shuffled data 80/10/10."""
+  split_1 = int(0.8 * len(filenames))
+  split_2 = int(0.9 * len(filenames))
+  train_images = filenames[:split_1]
+  dev_images = filenames[split_1:split_2]
+  test_images = filenames[split_2:]
+  return train_images, dev_images, test_images
+
+
+def get_and_shuffle(data_dir):
+  """Build a shuffled list of image names."""
+  image_name_list = []
+  for label_name in os.listdir(data_dir):
+    for image_name in os.listdir(os.path.join(data_dir, label_name)):
+      image_name_list.append(image_name)
+  # Shuffle the data
+  shuffled_index = list(range(len(image_name_list)))
+  random.shuffle(shuffled_index)
+  filenames = [image_name_list[i] for i in shuffled_index]
+  return shuffled_filename
+
+
+
---
+++ preprocess.py
@@ -0,0 +1,59 @@
+import numpy as np
+import tifffile as tiff
+import os
+
+"""Read tif files and convert to Keras friendly numpy arrays.
+  
+   returns data_dict with keys:
+      'train' (dict) with keys: 
+          'X_train' (np.array) train data with shape (21600, 64, 64, 13)
+          'Y_train' (np.array) train labels with shape (21600, 10) 
+      'dev' (dict) with keys:
+          'X_dev' (np.array) dev data with shape (2700, 64, 64, 13)
+          'Y_dev' (np.array) dev labels with shape (2700, 10)
+      'train' (dict) with keys: 
+          'X_test' (np.array) test data with shape (2700, 64, 64, 13)
+          'Y_test' (np.array) test labels with shape (2700, 10)
+"""
+
+train_path = '../satellite_data/train/'
+dev_path = '../satellite_data/dev/'
+test_path = '../satellite_data/test/'
+
+
+def main(train_path, dev_path, test_path):
+  label_dict, reverse_dict = create_label_mapping()
+  X_train, Y_train = get_data(train_path, label_dict) 
+  X_dev, Y_dev = get_data(dev_path, label_dict)
+  X_test, Y_test = get_data(test_path, label_dict)
+  data_dict = {}
+  data_dict['train'] = {'X_train': X_train, 'Y_train': Y_train}  
+  data_dict['dev'] = {'X_dev': X_dev, 'Y_dev': Y_dev}
+  data_dict['test'] = {'X_test': X_test, 'Y_test': Y_test}
+  return data_dict
+
+def create_label_mapping():
+  """Create dicts mapping string label to numeric label and vice versa.
+    Returns:
+      label_dict: (dict) mapping of string label to numeric label
+      reverse_dict: (dict) mapping of numeric label to string label
+  """
+  labels = ['Forest', 'River', 'Highway', 'AnnualCrop', 'SeaLake', 'HerbaceousVegetation', 'Industrial', 'Residential', 'PermanentCrop', 'Pasture'] 
+  label_dict = dict(zip(labels, np.arange(len(labels))))
+  reverse_dict = dict(map(reversed, label_dict.items()))
+  return label_dict, reverse_dict
+
+
+def get_data(data_path, label_dict):
+  X = []
+  Y = []
+  filenames = os.listdir(data_path)
+  for idx,name in enumerate(filenames):
+    image_data = tiff.imread(data_path + name)
+    X.append(image_data)
+    label = name.split('_')[0]
+    label_idx = label_dict[label]
+    Y.append(label_idx)
+  X = np.asarray(X)
+  Y = tf.keras.utils.to_categorical(Y)
+  return X, Y
